8102cd380e9b6751e450a5db557b2c35f09d3793
Dan Ober <77981895+daniel-ober@users.noreply.github.com>
2021-08-10 13:52:18 -0500

    basic layout with css

--- src/App.js
+++ src/App.js
@@ -1,31 +1,51 @@
-import React from 'react'
+import React from "react";
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
 
-const dateBuilder = (d) => {
-  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-  let day = days[d.getDay()];
-  let date = d.getDate();
-  let month = months[d.getMonth()];
-  let year = d.getFullYear();
-
-  return `${day} ${date} ${month} ${year}`
-}
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
-    <div className="app sunny">
+    <div className="app">
       <main>
-        <div className='search-box'>
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-          />
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search..." />
           <div className="location-box">
             <div className="location">Nashville, TN</div>
             <div className="date">{dateBuilder(new Date())}</div>
+          </div>
+          <div className="weather-box">
+            <div className="temp">95°</div>
+            <div className="weather">Sunny</div>
           </div>
         </div>
       </main>
